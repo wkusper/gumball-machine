0c371ccb76cc2c80e4afa022cdd077e04fd1bf1d
Hamidreza Bahramian <110060898+hbahramian@users.noreply.github.com>
2024-03-22 14:06:39 -0700

    Update IGumballMachine.java

--- src/main/java/edu/iu/habahram/GumballMachine/model/IGumballMachine.java
+++ src/main/java/edu/iu/habahram/GumballMachine/model/IGumballMachine.java
@@ -3,6 +3,5 @@ package edu.iu.habahram.GumballMachine.model;
 public interface IGumballMachine {
     TransitionResult insertQuarter();
     TransitionResult ejectQuarter();
-    TransitionResult turnCrank();
-    TransitionResult dispense();
+    TransitionResult turnCrank();    
 }
